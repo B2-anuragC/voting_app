905443b1df821a6c98026a059640fe2d0d73120a
Anurag <anurag.chauhan@iorta.in>
2023-10-25 10:12:52 +0530

    polls module added

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,16 +5,17 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const logger = new Logger('Main (main.ts)');
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: [
-        'http://localhost:8080',
-        /^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):8080$/,
-      ],
-    },
-  });
+  const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
+  const clientPort = configService.get("CLIENT_PORT");
+  app.enableCors({
+    origin: [
+      `http://localhost:${clientPort}`,
+       new RegExp(`/^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):${clientPort}$/`),
+    ]
+  })
+  
   const port = parseInt(configService.get('PORT'));
   await app.listen(port);
 
